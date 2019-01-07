c02778fc91611d2c5614041b8c8d162d8519f1a7
GingerBoredMan <adi18jan1999@gmail.com>
2019-01-07 16:32:04 +0530

    fixed MongoDB URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<GingerBoredMan>:<E@shaan2002>@ds151124.mlab.com:51124/todo' || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect('mongodb://<testuser1>:<testuser1>@ds151124.mlab.com:51124/todo' || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
 module.exports = {mongoose}
