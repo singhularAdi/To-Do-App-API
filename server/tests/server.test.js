dc4db511482c5aa13f490dc944f8abfd2fd4e308
GingerBoredMan <adi18jan1999@gmail.com>
2019-01-17 14:56:16 +0530

    Add token meathod

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -94,8 +94,45 @@ describe('GET /todos/:id', () => {
 
     it('should return 404 if non objectID', (done) => {
         request(app)
-        .get(`/todos/${ObjectID.isValid(1234)}`)
+        .get(`/todos/123`)
         .expect(404)
         .end(done)
     })
 });
+
+describe('/DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId)
+        })
+        .end((err,res) => {
+            if(err) {
+                return done(err);
+            }
+        });
+
+        Todo.findById(hexId).then((todo) => {
+            expect(todo).toNotExist();
+            done();
+        }).catch((e) => done(e));
+    });
+
+    it('should return a 404 if todo not found', (done) => {       
+        request(app)
+        .get(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done)
+    });
+
+    it('should return a 404 if object id is invalid', (done) => {
+        request(app)
+        .get(`/todos/1234`)
+        .expect(404)
+        .end(done)
+    });
+})
